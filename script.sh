392ac22ce8a40d1ec480a4417a799422d2e1e3f4
rajesh-ragam <74100130+rajesh-ragam@users.noreply.github.com>
2021-06-14 15:35:45 +0530

    Update script.sh
    removes $ as wrong

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "RUNNING THE SCRIPT NOW"
 echo "..........................."
-$OS=$(cat /etc/lsb-release)
+OS=$(cat /etc/lsb-release)
 echo "the os details are: $OS"
 echo "...................."
